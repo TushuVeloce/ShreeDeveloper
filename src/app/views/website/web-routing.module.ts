8b551d6f7c16b34f7b031677d7f3bb947bb89d7c
Tushar Deepak Chougule <tushar.c@velocetechinsights.com>
2025-01-23 14:21:49 +0530

    Master to Masters

--- src/app/views/website/web-routing.module.ts
+++ src/app/views/website/web-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WebComponent } from './web.component';
-import { MaterialMasterComponent } from './Master/material-master/material-master.component';
+import { MaterialMasterComponent } from './Masters/material-master/material-master.component';
 
 const routes: Routes = [
   {
