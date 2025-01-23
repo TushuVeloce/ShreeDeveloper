cd326808782ba1375c9bc8ae904123e66d2384aa
MaithileePatil <maithilee.p@velocetechinsights.com>
2025-01-23 17:03:30 +0530

    rotes and renamed folder names

--- src/app/views/website/web-routing.module.ts
+++ src/app/views/website/web-routing.module.ts
@@ -10,53 +10,53 @@ import { VendorMasterComponent } from './Masters/vendor-master/vendor-master.com
 import { VehicleMasterComponent } from './Masters/vehicle-master/vehicle-master.component';
 import { BankMasterComponent } from './Masters/bank-master/bank-master.component';
 import { UserMasterComponent } from './Masters/user-master/user-master.component';
-import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
-import { RegistrarOfficeComponent } from './Registrar Office/registrar-office/registrar-office.component';
-import { PlotResellByThirdPartyComponent } from './Plot Resell By Third Party/plot-resell-by-third-party/plot-resell-by-third-party.component';
-import { MarketingManagementComponent } from './Marketing Management/marketing-management/marketing-management.component';
-import { EmployeeManagementComponent } from './HR-Payroll Management/employee-management/employee-management.component';
-import { AttendanceAndSalaryComponent } from './HR-Payroll Management/attendance-and-salary/attendance-and-salary.component';
-import { AccountTransactionsComponent } from './Accounting/account-transactions/account-transactions.component';
-import { ExpenseTransactionsComponent } from './Accounting/expense-transactions/expense-transactions.component';
-import { ClientIncomeComponent } from './Accounting/Income Transactions/client-income/client-income.component';
-import { OfficeReportComponent } from './Reports/office-report/office-report.component';
-import { BookingReportComponent } from './Reports/booking-report/booking-report.component';
-import { StockReportComponent } from './Reports/stock-report/stock-report.component';
-import { CrmReportComponent } from './Reports/crm-report/crm-report.component';
-import { FollowUpReportComponent } from './Reports/follow-up-report/follow-up-report.component';
-import { EmployeeReportComponent } from './Reports/employee-report/employee-report.component';
-import { MarketingReportComponent } from './Reports/marketing-report/marketing-report.component';
-import { StagesReportComponent } from './Reports/stages-report/stages-report.component';
-import { AccountReportComponent } from './Reports/account-report/account-report.component';
-import { BillingReportComponent } from './Reports/billing-report/billing-report.component';
-import { GovernmentOfficeComponent } from './government Office/government-office/government-office.component';
+import { GovernmentOfficeComponent } from './government_office/government-office/government-office.component';
+import { RegistrarOfficeComponent } from './registrar_office/registrar-office/registrar-office.component';
+import { PlotResellByThirdPartyComponent } from './plot_resell_by_third_party/plot-resell-by-third-party/plot-resell-by-third-party.component';
+import { MarketingManagementComponent } from './marketing_management/marketing-management/marketing-management.component';
+import { EmployeeManagementComponent } from './hr_payroll_management/employee-management/employee-management.component';
+import { AttendanceAndSalaryComponent } from './hr_payroll_management/attendance-and-salary/attendance-and-salary.component';
+import { AccountTransactionsComponent } from './accounting/account-transactions/account-transactions.component';
+import { ExpenseTransactionsComponent } from './accounting/expense-transactions/expense-transactions.component';
+import { ClientIncomeComponent } from './accounting/Income Transactions/client-income/client-income.component';
+import { BillingReportComponent } from './reports/billing-report/billing-report.component';
+import { OfficeReportComponent } from './reports/office-report/office-report.component';
+import { BookingReportComponent } from './reports/booking-report/booking-report.component';
+import { StockReportComponent } from './reports/stock-report/stock-report.component';
+import { CrmReportComponent } from './reports/crm-report/crm-report.component';
+import { FollowUpReportComponent } from './reports/follow-up-report/follow-up-report.component';
+import { MarketingReportComponent } from './reports/marketing-report/marketing-report.component';
+import { StagesReportComponent } from './reports/stages-report/stages-report.component';
+import { AccountReportComponent } from './reports/account-report/account-report.component';
+import { EmployeeReportComponent } from './reports/employee-report/employee-report.component';
+import { DashboardComponent } from './Dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '', component: WebComponent,
     children: [
-      // {path: 'Dashboard', component: DashboardComponent},
-      { path: 'Material_Master', component: MaterialMasterComponent },
-      { path: 'Stage_Master', component: StageMasterComponent },
-      { path: 'Account_Main_Ledger', component: AccountMainLedgerComponent },
-      { path: 'Account_Sub_Ledger', component: AccountSubLedgerComponent },
-      { path: 'Marketing_Master', component: MarketingMasterComponent },
-      { path: 'Vendor_Master', component: VendorMasterComponent },
-      { path: 'Vehicle_Master', component: VehicleMasterComponent },
-      { path: 'Bank_Master', component: BankMasterComponent },
-      { path: 'User_Master', component: UserMasterComponent },
+      {path: 'Dashboard', component: DashboardComponent},
+      { path: 'Material_Master', component: MaterialMasterComponent},
+      { path: 'Stage_Master', component: StageMasterComponent},
+      { path: 'Account_Main_Ledger', component: AccountMainLedgerComponent},
+      { path: 'Account_Sub_Ledger', component: AccountSubLedgerComponent},
+      { path: 'Marketing_Master', component: MarketingMasterComponent},
+      { path: 'Vendor_Master', component: VendorMasterComponent},
+      { path: 'Vehicle_Master', component: VehicleMasterComponent},
+      { path: 'Bank_Master', component: BankMasterComponent},
+      { path: 'User_Master', component: UserMasterComponent},
 
 
-      { path: 'government _Office', component: GovernmentOfficeComponent },
+      { path: 'government _Office', component: GovernmentOfficeComponent},
       
-      { path: 'Registrar_Office', component: RegistrarOfficeComponent },
+      { path: 'Registrar_Office', component: RegistrarOfficeComponent},
 
-      { path: 'Plot_Resell_By_Third_Party', component: PlotResellByThirdPartyComponent },
+      { path: 'Plot_Resell_By_Third_Party', component: PlotResellByThirdPartyComponent},
 
-      { path: 'Marketing_Management', component: MarketingManagementComponent },
+      { path: 'Marketing_Management', component: MarketingManagementComponent},
 
-      { path: 'Employee_Management', component: EmployeeManagementComponent },
-      { path: 'Attendance_and_Salary', component: AttendanceAndSalaryComponent },
+      { path: 'Employee_Management', component: EmployeeManagementComponent},
+      { path: 'Attendance_and_Salary', component: AttendanceAndSalaryComponent},
 
       { path: 'Accounting_Transaction', component: AccountTransactionsComponent},
       { path: 'Expense_Transactions', component: ExpenseTransactionsComponent},
