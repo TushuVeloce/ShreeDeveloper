6b2c1909f6d6160281d94ae0f2da3ef566811595
Tushar Deepak Chougule <tushar.c@velocetechinsights.com>
2025-01-24 14:25:26 +0530

    Update material-master.component.ts

--- src/app/views/website/Masters/material-master/material-master.component.ts
+++ src/app/views/website/Masters/material-master/material-master.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-material-master',
+  standalone: false,
   templateUrl: './material-master.component.html',
   styleUrls: ['./material-master.component.scss'],
 })
