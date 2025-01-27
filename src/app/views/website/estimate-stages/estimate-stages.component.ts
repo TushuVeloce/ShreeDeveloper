360cb6248a895c9167087d16ffe57501ebdb21c0
Tushar Deepak Chougule <tushar.c@velocetechinsights.com>
2025-01-27 15:59:54 +0530

    Update estimate-stages.component.ts

--- src/app/views/website/estimate-stages/estimate-stages.component.ts
+++ src/app/views/website/estimate-stages/estimate-stages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-estimate-stages',
+  standalone: false,
   templateUrl: './estimate-stages.component.html',
   styleUrls: ['./estimate-stages.component.scss'],
 })
