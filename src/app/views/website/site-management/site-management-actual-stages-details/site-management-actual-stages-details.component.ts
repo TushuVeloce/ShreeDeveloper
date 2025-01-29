1f681f61d695d79771cb431995451e5ef91725f1
UmarMulla17 <99394160+umarmulla@users.noreply.github.com>
2025-01-29 16:30:32 +0530

    code changes

--- src/app/views/website/site-management/site-management-actual-stages-details/site-management-actual-stages-details.component.ts
+++ src/app/views/website/site-management/site-management-actual-stages-details/site-management-actual-stages-details.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-site-management-actual-stages-details',
   templateUrl: './site-management-actual-stages-details.component.html',
+  standalone: false,
   styleUrls: ['./site-management-actual-stages-details.component.scss'],
 })
 export class SiteManagementActualStagesDetailsComponent  implements OnInit {
 
   constructor() { }
+
+  dieselLtr: number = 0;
+  amountPerLtr: number = 0;
+  totalAmount: number = 0;
+
+  calculateTotal() {
+    this.totalAmount = (this.dieselLtr * this.amountPerLtr);
+  }
 
   ngOnInit() {}
 
