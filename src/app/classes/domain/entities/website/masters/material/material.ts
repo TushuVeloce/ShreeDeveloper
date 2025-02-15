708fb9893874f40901fc9fec233f68eea8e7bb52
MaithileePatil <maithilee.p@velocetechinsights.com>
2025-02-15 13:07:29 +0530

    Unit

--- src/app/classes/domain/entities/website/masters/material/material.ts
+++ src/app/classes/domain/entities/website/masters/material/material.ts
@@ -64,7 +64,7 @@ export class Material implements IPersistable<Material> {
   public CheckSaveValidity(_td: TransportData, vra: ValidationResultAccumulator): void {
     if (!this.AllowEdit) vra.add('', 'This object is not editable and hence cannot be saved.');
     if (this.p.Name == '') vra.add('Name', 'Name cannot be blank.');
-    if (this.p.UnitName == '') vra.add('Unit', 'Unit cannot be blank.');
+    if (this.p.UnitRef == 0) vra.add('Unit', 'Unit cannot be blank.');
   }
 
   public MergeIntoTransportData(td: TransportData) {
