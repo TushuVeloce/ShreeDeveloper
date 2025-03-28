95f8242b8a0a04504ddd9f23bf1ef90e34c115ac
Tushar Deepak Chougule <tushar.c@velocetechinsights.com>
2025-03-28 16:24:02 +0530

    registrar office

--- src/app/views/website/registrar_office/registrar-office-detail/registrar-office-detail.component.ts
+++ src/app/views/website/registrar_office/registrar-office-detail/registrar-office-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Department } from 'src/app/classes/domain/entities/website/masters/department/department';
+import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
+import { UIUtils } from 'src/app/services/uiutils.service';
+import { Utils } from 'src/app/services/utils.service';
+
 
 @Component({
   selector: 'app-registrar-office-detail',
@@ -7,9 +14,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-office-detail.component.scss'],
 })
 export class RegistrarOfficeDetailComponent  implements OnInit {
+  isSaveDisabled: boolean = false;
+  private IsNewEntity: boolean = true;
+  isChecked = false; // Default value
+  Entity: Department = Department.CreateNewInstance();
+  DetailsFormTitle: 'New Registrar Office' | 'Edit Registrar Office' = 'New Registrar Office';
+  InitialEntity: Department = null as any;
+  companyName = this.companystatemanagement.SelectedCompanyName;
+  constructor(private router: Router, private uiUtils: UIUtils, private appStateManage: AppStateManageService, private utils: Utils,private companystatemanagement: CompanyStateManagement) { }
 
-  constructor() { }
+  async ngOnInit() {
+    this.appStateManage.setDropdownDisabled(true);
+       if (this.appStateManage.StorageKey.getItem('Editable') == 'Edit') {
+          this.IsNewEntity = false;
+          this.DetailsFormTitle = this.IsNewEntity ? 'New Registrar Office' : 'Edit Registrar Office';
+          this.Entity = Department.GetCurrentInstance();
+          this.appStateManage.StorageKey.removeItem('Editable')
+    
+        } else {
+          this.Entity = Department.CreateNewInstance();
+          Department.SetCurrentInstance(this.Entity);
+         
+        }
+        this.InitialEntity = Object.assign(Department.CreateNewInstance(),
+        this.utils.DeepCopy(this.Entity)) as Department;      
+   }
 
-  ngOnInit() {}
+    SaveDepartmentMaster = async () => {
+      this.Entity.p.CompanyRef = this.companystatemanagement.getCurrentCompanyRef()
+      this.Entity.p.CompanyName = this.companystatemanagement.getCurrentCompanyName()
+      let entityToSave = this.Entity.GetEditableVersion();
+      console.log('entityToSave :', entityToSave);
+      let entitiesToSave = [entityToSave]
+      // await this.Entity.EnsurePrimaryKeysWithValidValues()
+      let tr = await this.utils.SavePersistableEntities(entitiesToSave);
+      if (!tr.Successful) {
+        this.isSaveDisabled = false;
+        this.uiUtils.showErrorMessage('Error',tr.Message);
+        return
+      }
+      else {
+        this.isSaveDisabled = false;
+        // this.onEntitySaved.emit(entityToSave);
+        if (this.IsNewEntity) {
+          await this.uiUtils.showSuccessToster('Registrar Office saved successfully!');
+          this.Entity = Department.CreateNewInstance();
+        } else {
+          await this.router.navigate(['/homepage/Website/Department_Master'])
+          await this.uiUtils.showSuccessToster('Department Updated successfully!');
+        }
+      }
+    }
+
+  BackDepartment() {
+    this.router.navigate(['/homepage/Website/Department_Master']);
+  }
 
 }
--- src/app/views/website/registrar_office/registrar-office/registrar-office.component.ts
+++ src/app/views/website/registrar_office/registrar-office/registrar-office.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,effect  } from '@angular/core';
+import { Router } from '@angular/router';
+import { Department } from 'src/app/classes/domain/entities/website/masters/department/department';
+import { Site } from 'src/app/classes/domain/entities/website/masters/site/site';
+import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
+import { UIUtils } from 'src/app/services/uiutils.service';
 
 @Component({
   selector: 'app-registrar-office',
@@ -8,8 +14,116 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarOfficeComponent  implements OnInit {
 
-  constructor() { }
+  Entity: Department = Department.CreateNewInstance();
+  MasterList: Department[] = [];
+  DisplayMasterList: Department[] = [];
+  SiteList: Site[] = [];
+  SearchString: string = '';
+  SelectedDepartment: Department = Department.CreateNewInstance();
+  pageSize = 10; // Items per page
+  currentPage = 1; // Initialize current page
+  total = 0;
+  companyRef = this.companystatemanagement.SelectedCompanyRef;
+  
+  headers: string[] = ['Sr.No.', 'Name', 'Company Name', 'Action'];
+  
+  constructor(private uiUtils: UIUtils, private router: Router, private appStateManage: AppStateManageService,private companystatemanagement: CompanyStateManagement) {
+    effect(() => {
+      this.getDepartmentListByCompanyRef()
+    });
+   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    // await this.FormulateMasterList();
+    this.appStateManage.setDropdownDisabled(false);
+    this.loadPaginationData();
+  }
+  
+  // private FormulateMasterList = async () => {
+  //   let lst = await Department.FetchEntireList(async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+  //   this.MasterList = lst;
+  //   this.DisplayMasterList = this.MasterList
+  //   this.loadPaginationData();
+  // }
+
+   getDepartmentListByCompanyRef = async () => {
+      this.MasterList = [];
+      this.DisplayMasterList = [];
+      if(this.companyRef()<=0){
+        await this.uiUtils.showErrorToster('Company not Selected');
+        return;
+      }
+        let lst = await Department.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+        this.MasterList = lst;
+        this.DisplayMasterList = this.MasterList;
+      this.loadPaginationData();
+    }
+    // FormulateSiteListByCompanyRef = async () => {
+    //   this.MasterList = [];
+    //   this.DisplayMasterList = [];
+    //   this.SiteList = [];
+    //   this.Entity.p.SiteManagementRef = 0;
+    //   if (this.companyRef() <= 0) {
+    //     await this.uiUtils.showErrorToster('Company not Selected');
+    //     return;
+    //   }
+    //   let lst = await Site.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    //   this.SiteList = lst;
+    //   this.loadPaginationData();
+    // }
+  onEditClicked = async (item: Department) => {
+    this.SelectedDepartment = item.GetEditableVersion();
+    Department.SetCurrentInstance(this.SelectedDepartment);
+    this.appStateManage.StorageKey.setItem('Editable', 'Edit');
+    await this.router.navigate(['/homepage/Website/Registrar_Office_Details']);
+  }
+
+  onDeleteClicked = async (Department: Department) => {
+    await this.uiUtils.showConfirmationMessage('Delete',
+      `This process is <strong>IRREVERSIBLE!</strong> <br/>
+    Are you sure that you want to DELETE this Registrar Office?`,
+      async () => {
+        await Department.DeleteInstance(async () => {
+          await this.uiUtils.showSuccessToster(`Registrar Office ${Department.p.Name} has been deleted!`);
+          this.SearchString = '';
+          this.loadPaginationData();
+        await this.getDepartmentListByCompanyRef();
+        });
+      });
+  }
+
+  // For Pagination  start ----
+  loadPaginationData = () => {
+    this.total = this.DisplayMasterList.length; // Update total based on loaded data
+  }
+
+  get paginatedList () {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.DisplayMasterList.slice(start, start + this.pageSize);
+  }
+
+  onPageChange  = (pageIndex: number): void => {
+    this.currentPage = pageIndex; // Update the current page
+  }
+
+  async AddDepartment() {
+    if (this.companyRef() <= 0) {
+      this.uiUtils.showErrorToster('Company not Selected');
+      return;
+    }
+    this.router.navigate(['/homepage/Website/Registrar_Office_Details']);
+  }
+
+  filterTable = () => {
+    if (this.SearchString != '') {
+      this.DisplayMasterList = this.MasterList.filter((data: any) => {
+        return data.p.Name.toLowerCase().indexOf(this.SearchString.toLowerCase()) > -1 ||
+         data.p.CompanyName.toLowerCase().indexOf(this.SearchString.toLowerCase()) > -1
+      })
+    }
+    else {
+      this.DisplayMasterList = this.MasterList
+    }
+  }
 
 }
