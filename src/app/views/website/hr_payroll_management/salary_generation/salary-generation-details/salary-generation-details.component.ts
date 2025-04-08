f781d342a792cdbb6c4a54dfd1a6096fb85b65b5
MaithileePatil <maithilee.p@velocetechinsights.com>
2025-04-08 17:32:56 +0530

    enum

--- src/app/views/website/hr_payroll_management/salary_generation/salary-generation-details/salary-generation-details.component.ts
+++ src/app/views/website/hr_payroll_management/salary_generation/salary-generation-details/salary-generation-details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DomainEnums } from 'src/app/classes/domain/domainenums/domainenums';
+import { Employee } from 'src/app/classes/domain/entities/website/masters/employee/employee';
+import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
+import { ScreenSizeService } from 'src/app/services/screensize.service';
+import { UIUtils } from 'src/app/services/uiutils.service';
+import { Utils } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-salary-generation-details',
@@ -7,9 +15,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./salary-generation-details.component.scss'],
 })
 export class SalaryGenerationDetailsComponent  implements OnInit {
+    isSaveDisabled: boolean = false;
+    private IsNewEntity: boolean = true;
+    // Entity: EmployeeExit = EmployeeExit.CreateNewInstance();
+    DetailsFormTitle: 'New Employee Exit' | 'Edit Employee Exit' = 'New Employee Exit';
+    EmployeeList: Employee[] = [];
+    MonthList = DomainEnums.MonthList(true, '---Select Month---');
+    companyName = this.companystatemanagement.SelectedCompanyName;
+    companyRef = this.companystatemanagement.SelectedCompanyRef;
+   constructor(private router: Router, private uiUtils: UIUtils, private appStateManage: AppStateManageService, private utils: Utils, private companystatemanagement: CompanyStateManagement) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.getEmployeeListByCompanyRef()
 
-  ngOnInit() {}
+  }
 
+  getEmployeeListByCompanyRef = async () => {
+    let lst = await Employee.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    this.EmployeeList = lst;
+  }
+
+  async BackOfficeTime() {
+    this.router.navigate(['/homepage/Website/Office_Duty_Time']);
+  }
 }
