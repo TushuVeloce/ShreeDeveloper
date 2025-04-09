29bcd219d8a7d92a9f2798599d9c2e01606d2799
MaithileePatil <maithilee.p@velocetechinsights.com>
2025-04-09 14:36:15 +0530

    hr and payroll

---
+++ src/app/classes/domain/entities/website/HR_and_Payroll/Salary_Generation/salarygeneration.ts
@@ -0,0 +1,204 @@
+import { IPersistable } from "src/app/classes/infrastructure/IPersistable";
+import { DataContainer } from "src/app/classes/infrastructure/datacontainer";
+import { DataContainerService } from "src/app/classes/infrastructure/datacontainer.service";
+import { PayloadPacketFacade } from "src/app/classes/infrastructure/payloadpacket/payloadpacketfacade";
+import { TransportData } from "src/app/classes/infrastructure/transportdata";
+import { ValidationResultAccumulator } from "src/app/classes/infrastructure/validationresultaccumulator";
+import { IdProvider } from "src/app/services/idprovider.service";
+import { ServerCommunicatorService } from "src/app/services/server-communicator.service";
+import { Utils } from "src/app/services/utils.service";
+import { isNullOrUndefined } from "src/tools";
+import { UIUtils } from "src/app/services/uiutils.service";
+import { RequestTypes } from "src/app/classes/infrastructure/enums";
+import { SalaryGenerationFetchRequest } from "./salarygenerationfetchrequest";
+
+
+export class SalaryGenerationProps {
+  public readonly Db_Table_Name = "SalaryGenerationMaster";
+  public Ref: number = 0;
+  public EmployeeNRef: string = '';
+  public EmployeeName: string = '';
+  public Month: string = '';
+  public TotalDays: number = 0;
+  public TotalWorkingDays: number = 0;
+  public TotalLeaves: number = 0;
+  public TotalOverTimeHours: number = 0;
+  public TotalWorkingHours: number = 0;
+  public TotalLeavesHours: number = 0;
+  public OverallWorkingHours: number = 0;
+  public BasicSalary: number = 0;
+  public TotalAllowance: number = 0;
+  public TotalIncentive: number = 0;
+  public Other: number = 0;
+  public GrossTotal: number = 0;
+  public TDS: number = 0;
+  public PF: number = 0;
+  public TotalLeaveDeduction: number = 0;
+  public AdvancePayment: number = 0;
+  public TotalDeduction: number = 0;
+  public NetSalary: number = 0;
+  public CompanyRef: number = 0;
+  public CompanyName: string = '';
+  public CreatedBy: number = 0;
+  public UpdatedBy: number = 0;
+
+  public readonly IsNewlyCreated: boolean = false;
+  // public readonly AccountTypeName: string = '';
+
+  private constructor(isNewlyCreated: boolean) {
+    this.IsNewlyCreated = isNewlyCreated;
+  }
+
+  public static Blank() {
+    return new SalaryGenerationProps(true);
+  }
+}
+
+export class SalaryGeneration implements IPersistable<SalaryGeneration> {
+  public static readonly Db_Table_Name: string = 'SalaryGenerationMaster';
+
+  private constructor(public readonly p: SalaryGenerationProps, public readonly AllowEdit: boolean) {
+
+  }
+
+  public async EnsurePrimaryKeysWithValidValues(): Promise<void> {
+    if (this.p.Ref === undefined || this.p.Ref === 0) {
+            const newRefs = await IdProvider.GetInstance().GetNextEntityId();
+            // const newRefs = await IdProvider.GetInstance().GetAllocateSingleIds();
+      this.p.Ref = newRefs[0];
+      if (this.p.Ref <= 0) throw new Error("Cannot assign Id. Please try again");
+    }
+  }
+
+  public GetEditableVersion(): SalaryGeneration {
+    let newState: SalaryGenerationProps = Utils.GetInstance().DeepCopy(this.p);
+    return SalaryGeneration.CreateInstance(newState, true);
+  }
+
+  public static CreateNewInstance() {
+    return new SalaryGeneration(SalaryGenerationProps.Blank(), true);
+  }
+
+  public static CreateInstance(data: any, allowEdit: boolean) {
+    return new SalaryGeneration(data as SalaryGenerationProps, allowEdit);
+  }
+
+  public CheckSaveValidity(_td: TransportData, vra: ValidationResultAccumulator): void {
+    if (!this.AllowEdit) vra.add('', 'This object is not editable and hence cannot be saved.');
+  }
+
+  public MergeIntoTransportData(td: TransportData) {
+    DataContainerService.GetInstance().MergeIntoContainer(td.MainData, SalaryGeneration.Db_Table_Name, this.p);
+  }
+
+  private static m_currentInstance: SalaryGeneration = SalaryGeneration.CreateNewInstance();
+
+  public static GetCurrentInstance() {
+    return SalaryGeneration.m_currentInstance;
+  }
+
+  public static SetCurrentInstance(value: SalaryGeneration) {
+    SalaryGeneration.m_currentInstance = value;
+  }
+
+
+  // ********************************************
+  public static cacheDataChangeLevel: number = -1;
+
+  public static SingleInstanceFromTransportData(td: TransportData): SalaryGeneration {
+    let dcs = DataContainerService.GetInstance();
+    if (dcs.CollectionExists(td.MainData, SalaryGeneration.Db_Table_Name)) {
+      for (let data of dcs.GetCollection(td.MainData, SalaryGeneration.Db_Table_Name)!.Entries) {
+        return SalaryGeneration.CreateInstance(data, false);
+      }
+    }
+
+    return null as any;
+  }
+
+  public static ListFromDataContainer(cont: DataContainer,
+    filterPredicate: (arg0: any) => boolean = null as any,
+    sortPropertyName: string = "Name"): SalaryGeneration[] {
+    let result: SalaryGeneration[] = [];
+
+    let dcs = DataContainerService.GetInstance();
+
+    if (dcs.CollectionExists(cont, SalaryGeneration.Db_Table_Name)) {
+      let coll = dcs.GetCollection(cont, SalaryGeneration.Db_Table_Name)!;
+      let entries = coll.Entries;
+
+      if (!isNullOrUndefined(filterPredicate)) entries = entries.filter(filterPredicate);
+
+      if (sortPropertyName.trim().length > 0) {
+        entries = Utils.OrderByPropertyName(entries, sortPropertyName);
+      }
+
+      for (let data of entries) {
+        result.push(SalaryGeneration.CreateInstance(data, false));
+      }
+    }
+
+    return result;
+  }
+
+  public static ListFromTransportData(td: TransportData): SalaryGeneration[] {
+    return SalaryGeneration.ListFromDataContainer(td.MainData);
+  }
+
+  public static async FetchTransportData(req: SalaryGenerationFetchRequest, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdRequest = req.FormulateTransportData();
+    let pktRequest = PayloadPacketFacade.GetInstance().CreateNewPayloadPacket2(tdRequest);
+
+    let tr = await ServerCommunicatorService.GetInstance().sendHttpRequest(pktRequest);
+    if (!tr.Successful) {
+      if (!isNullOrUndefined(errorHandler)) await errorHandler(tr.Message);
+      return null;
+    }
+
+    let tdResponse = JSON.parse(tr.Tag) as TransportData;
+    return tdResponse;
+  }
+
+  public static async FetchInstance(ref: number, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let req = new SalaryGenerationFetchRequest();
+    req.SalaryGenerationRefs.push(ref);
+
+    let tdResponse = await SalaryGeneration.FetchTransportData(req, errorHandler) as TransportData;
+    return SalaryGeneration.SingleInstanceFromTransportData(tdResponse);
+  }
+
+  public static async FetchList(req: SalaryGenerationFetchRequest, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdResponse = await SalaryGeneration.FetchTransportData(req, errorHandler) as TransportData;
+    return SalaryGeneration.ListFromTransportData(tdResponse);
+  }
+
+  public static async FetchEntireList(errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let req = new SalaryGenerationFetchRequest();
+    let tdResponse = await SalaryGeneration.FetchTransportData(req, errorHandler) as TransportData;
+    return SalaryGeneration.ListFromTransportData(tdResponse);
+  }
+
+ public static async FetchEntireListByCompanyRef(CompanyRef:number,errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let req = new SalaryGenerationFetchRequest();
+    req.CompanyRefs.push(CompanyRef)
+    let tdResponse = await SalaryGeneration.FetchTransportData(req, errorHandler) as TransportData;
+    return SalaryGeneration.ListFromTransportData(tdResponse);
+  }
+
+  public async DeleteInstance(successHandler: () => Promise<void> = null!, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdRequest = new TransportData();
+    tdRequest.RequestType = RequestTypes.Deletion;
+
+    this.MergeIntoTransportData(tdRequest);
+    let pktRequest = PayloadPacketFacade.GetInstance().CreateNewPayloadPacket2(tdRequest);
+
+    let tr = await ServerCommunicatorService.GetInstance().sendHttpRequest(pktRequest);
+    if (!tr.Successful) {
+      if (!isNullOrUndefined(errorHandler)) await errorHandler(tr.Message);
+    }
+    else {
+      if (!isNullOrUndefined(successHandler)) await successHandler();
+    }
+  }
+
+}
--- src/app/views/website/hr_payroll_management/salary_generation/salary-generation/salary-generation.component.ts
+++ src/app/views/website/hr_payroll_management/salary_generation/salary-generation/salary-generation.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SalaryGeneration } from 'src/app/classes/domain/entities/website/HR_and_Payroll/Salary_Generation/salarygeneration';
 import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
 import { ScreenSizeService } from 'src/app/services/screensize.service';
 import { UIUtils } from 'src/app/services/uiutils.service';
 
@@ -11,31 +13,102 @@ import { UIUtils } from 'src/app/services/uiutils.service';
   styleUrls: ['./salary-generation.component.scss'],
 })
 export class SalaryGenerationComponent  implements OnInit {
+  Entity: SalaryGeneration = SalaryGeneration.CreateNewInstance();
+  MasterList: SalaryGeneration[] = [];
+  DisplayMasterList: SalaryGeneration[] = [];
+  SearchString: string = '';
+  SelectedSalary: SalaryGeneration = SalaryGeneration.CreateNewInstance();
+  CustomerRef: number = 0;
+  pageSize = 10; // Items per page
+  currentPage = 1; // Initialize current page
+  total = 0;
 
-    pageSize = 10; // Items per page
-    currentPage = 1; // Initialize current page
-    total = 0;
-    SearchString: string = '';
-  
-    constructor(private uiUtils: UIUtils, private router: Router, private appStateManage: AppStateManageService, private screenSizeService: ScreenSizeService)
-    { }
-  
-    ngOnInit() {}
-  
-    headers: string[] = ['Sr.No.', 'Code', 'Material Name', 'Material Unit', 'Action'];
-  
-  
-    onPageChange = (pageIndex: number): void => {
-      this.currentPage = pageIndex; // Update the current page
-    };
-  
-    async AddSalaryDetails() {
-      this.router.navigate(['/homepage/Website/Salary_Generation_Details']);
-    }
-  
-  
-    filterTable = () => {
-  
-    }
+  companyRef = this.companystatemanagement.SelectedCompanyRef;
 
+  headers: string[] = ['Sr.No.', 'Employee Name', 'Month', 'Total Working Days','Basic Salary','Gross Total','Total Deduction','Net Salary', 'Action'];
+  constructor(private uiUtils: UIUtils, private router: Router, private appStateManage: AppStateManageService, private screenSizeService: ScreenSizeService,
+    private companystatemanagement: CompanyStateManagement
+  ) {
+    effect(async () => {
+      await this.getSalaryListByCompanyRef();
+    });
+  }
+
+  async ngOnInit() {
+    this.appStateManage.setDropdownDisabled(false);
+    this.loadPaginationData();
+    this.pageSize = this.screenSizeService.getPageSize('withoutDropdown');
+  }
+
+  getSalaryListByCompanyRef = async () => {
+    this.MasterList = [];
+    this.DisplayMasterList = [];
+    if (this.companyRef() <= 0) {
+      await this.uiUtils.showErrorToster('Company not Selected');
+      return;
+    }
+    let lst = await SalaryGeneration.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    this.MasterList = lst;
+
+    this.DisplayMasterList = this.MasterList;
+    this.loadPaginationData();
+  }
+
+  onEditClicked = async (item: SalaryGeneration) => {
+    this.SelectedSalary = item.GetEditableVersion();
+    SalaryGeneration.SetCurrentInstance(this.SelectedSalary);
+    this.appStateManage.StorageKey.setItem('Editable', 'Edit');
+    await this.router.navigate(['/homepage/Website/Material_Master_Details']);
+  };
+
+  onDeleteClicked = async (SalaryGeneration: SalaryGeneration) => {
+    await this.uiUtils.showConfirmationMessage(
+      'Delete',
+      `This process is <strong>IRREVERSIBLE!</strong> <br/>
+    Are you sure that you want to DELETE this Salary Generation?`,
+      async () => {
+        await SalaryGeneration.DeleteInstance(async () => {
+          await this.uiUtils.showSuccessToster(
+            `Salary Generation of ${SalaryGeneration.p.EmployeeName} has been deleted!`
+          );
+          await this.getSalaryListByCompanyRef();
+          this.SearchString = '';
+          this.loadPaginationData();
+        });
+      }
+    );
+  };
+
+  // For Pagination  start ----
+  loadPaginationData = () => {
+    this.total = this.DisplayMasterList.length; // Update total based on loaded data
+  };
+
+  paginatedList = () => {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.DisplayMasterList.slice(start, start + this.pageSize);
+  }
+
+  onPageChange = (pageIndex: number): void => {
+    this.currentPage = pageIndex; // Update the current page
+  };
+
+  AddSalaryDetails = () => {
+    if (this.companyRef() <= 0) {
+      this.uiUtils.showErrorToster('Company not Selected');
+      return;
+    }
+    this.router.navigate(['/homepage/Website/Salary_Generation_Details']);
+  }
+
+  filterTable = () => {
+    if (this.SearchString != '') {
+      this.DisplayMasterList = this.MasterList.filter((data: any) => {
+        return data.p.Name.toLowerCase().indexOf(this.SearchString.toLowerCase()) > -1
+      })
+    }
+    else {
+      this.DisplayMasterList = this.MasterList
+    }
+  }
 }
