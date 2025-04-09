3799700f6e02e79a5e35ed508a2efa406e07788e
Ijaj97 <124039753+Ijaj97@users.noreply.github.com>
2025-04-09 14:52:54 +0530

    leave & salary slip approval

--- src/app/views/website/hr_payroll_management/salary-slip-approval/salary-slip-approval.component.ts
+++ src/app/views/website/hr_payroll_management/salary-slip-approval/salary-slip-approval.component.ts
@@ -1,15 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SalarySlipRequest } from 'src/app/classes/domain/entities/website/request/salarysliprequest/salarysliprequest';
+import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
+import { ScreenSizeService } from 'src/app/services/screensize.service';
+import { UIUtils } from 'src/app/services/uiutils.service';
+import { Employee } from 'src/app/classes/domain/entities/website/masters/employee/employee';
 
 @Component({
   selector: 'app-salary-slip-approval',
   standalone: false,
   templateUrl: './salary-slip-approval.component.html',
   styleUrls: ['./salary-slip-approval.component.scss'],
+
 })
-export class SalarySlipApprovalComponent  implements OnInit {
 
-  constructor() { }
+export class SalarySlipApprovalComponent implements OnInit {
+  Entity: SalarySlipRequest = SalarySlipRequest.CreateNewInstance();
+  MasterList: SalarySlipRequest[] = [];
+  DisplayMasterList: SalarySlipRequest[] = [];
+  SearchString: string = '';
+  SelectedSalarySlipApproval: SalarySlipRequest = SalarySlipRequest.CreateNewInstance();
+  CustomerRef: number = 0;
+  EmployeeList: Employee[] = [];
+  pageSize = 10; // Items per page
+  currentPage = 1; // Initialize current page
+  total = 0;
 
-  ngOnInit() {}
+  companyRef = this.companystatemanagement.SelectedCompanyRef;
 
+  headers: string[] = ['Sr.No.', 'From Month', 'To Month', 'From Year', 'To Year', 'Is Approved', 'Action'];
+  constructor(private uiUtils: UIUtils, private router: Router, private appStateManage: AppStateManageService, private screenSizeService: ScreenSizeService,
+    private companystatemanagement: CompanyStateManagement
+  ) {
+    effect(async () => {
+      await this.getEmployeeListByCompanyRef();
+    });
+  }
+
+  async ngOnInit() {
+    this.appStateManage.setDropdownDisabled(false);
+    this.loadPaginationData();
+    this.pageSize = this.screenSizeService.getPageSize('withoutDropdown');
+  }
+
+  getEmployeeListByCompanyRef = async () => {
+    if (this.companyRef() <= 0) {
+      await this.uiUtils.showErrorToster('Company not Selected');
+      return;
+    }
+    let lst = await Employee.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    this.EmployeeList = lst;
+    this.Entity.p.EmployeeRef = this.EmployeeList[0].p.Ref
+  }
+
+  getSalarySlipApprovalListByEmployeeRef = async () => {
+    this.MasterList = [];
+    this.DisplayMasterList = [];
+    if (this.Entity.p.EmployeeRef <= 0) {
+      await this.uiUtils.showErrorToster('Employee not Selected');
+      return;
+    }
+    let lst = await SalarySlipRequest.FetchEntireListByEmployeeRef(this.Entity.p.EmployeeRef, async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    this.MasterList = lst;
+
+    this.DisplayMasterList = this.MasterList;
+    this.loadPaginationData();
+  }
+
+  onEditClicked = async (item: SalarySlipRequest) => {
+    this.SelectedSalarySlipApproval = item.GetEditableVersion();
+    SalarySlipRequest.SetCurrentInstance(this.SelectedSalarySlipApproval);
+    this.appStateManage.StorageKey.setItem('Editable', 'Edit');
+    await this.router.navigate(['/homepage/Website/Salary_Slip_Request_Details']);
+  };
+
+  onDeleteClicked = async (salaryslipapproval: SalarySlipRequest) => {
+    await this.uiUtils.showConfirmationMessage(
+      'Delete',
+      `This process is <strong>IRREVERSIBLE!</strong> <br/>
+    Are you sure that you want to DELETE this Salary Slip Approval?`,
+      async () => {
+        await salaryslipapproval.DeleteInstance(async () => {
+          await this.uiUtils.showSuccessToster(
+            `Salary Slip Approval ${salaryslipapproval.p.EmployeeName} has been deleted!`
+          );
+          await this.getSalarySlipApprovalListByEmployeeRef();
+          this.SearchString = '';
+          this.loadPaginationData();
+        });
+      }
+    );
+  };
+
+  // For Pagination  start ----
+  loadPaginationData = () => {
+    this.total = this.DisplayMasterList.length; // Update total based on loaded data
+  };
+
+  paginatedList = () => {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return this.DisplayMasterList.slice(start, start + this.pageSize);
+  }
+
+  onPageChange = (pageIndex: number): void => {
+    this.currentPage = pageIndex; // Update the current page
+  };
+
+  filterTable = () => {
+    if (this.SearchString != '') {
+      this.DisplayMasterList = this.MasterList.filter((data: any) => {
+        return data.p.Name.toLowerCase().indexOf(this.SearchString.toLowerCase()) > -1
+      })
+    }
+    else {
+      this.DisplayMasterList = this.MasterList
+    }
+  }
 }
--- src/app/views/website/request/salary_slip_request/salary-slip-request-details/salary-slip-request-details.component.ts
+++ src/app/views/website/request/salary_slip_request/salary-slip-request-details/salary-slip-request-details.component.ts
@@ -1,15 +1,142 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/classes/domain/entities/website/masters/employee/employee';
+import { AppStateManageService } from 'src/app/services/app-state-manage.service';
+import { CompanyStateManagement } from 'src/app/services/companystatemanagement';
+import { UIUtils } from 'src/app/services/uiutils.service';
+import { Utils } from 'src/app/services/utils.service';
+import { ValidationMessages, ValidationPatterns } from 'src/app/classes/domain/constants';
+import { NgModel } from '@angular/forms';
+import { SalarySlipRequest } from 'src/app/classes/domain/entities/website/request/salarysliprequest/salarysliprequest';
+import { DomainEnums } from 'src/app/classes/domain/domainenums/domainenums';
+import { DTU } from 'src/app/services/dtu.service';
+
 
 @Component({
-  selector: 'app-salary-slip-request-details',
+  selector: 'app-salarysliprequest-master-details',
   standalone: false,
   templateUrl: './salary-slip-request-details.component.html',
   styleUrls: ['./salary-slip-request-details.component.scss'],
 })
-export class SalarySlipRequestDetailsComponent  implements OnInit {
+export class SalarySlipRequestDetailsComponent implements OnInit {
+  Entity: SalarySlipRequest = SalarySlipRequest.CreateNewInstance();
+  private IsNewEntity: boolean = true;
+  isSaveDisabled: boolean = false;
+  DetailsFormTitle: 'New Salary Slip Request' | 'Edit Salary Slip Request' = 'New Salary Slip Request';
+  IsDropdownDisabled: boolean = false;
+  InitialEntity: SalarySlipRequest = null as any;
+  EmployeeList: Employee[] = [];
+  companyRef = this.companystatemanagement.SelectedCompanyRef;
+  frommonth: string = '';
+  tomonth: string = '';
+  fromyear: string = '';
+  toyear: string = '';
 
-  constructor() { }
+  NameWithNos: string = ValidationPatterns.NameWithNos
 
-  ngOnInit() {}
+  NameWithNosMsg: string = ValidationMessages.NameWithNosMsg
+  RequiredFieldMsg: string = ValidationMessages.RequiredFieldMsg
 
+  @ViewChild('NameCtrl') NameInputControl!: NgModel;
+  @ViewChild('CodeCtrl') CodeInputControl!: NgModel;
+
+  constructor(
+    private router: Router,
+    private uiUtils: UIUtils,
+    private appStateManage: AppStateManageService,
+    private utils: Utils,
+    private dtu: DTU,
+    private companystatemanagement: CompanyStateManagement
+  ) { }
+
+  async ngOnInit() {
+    this.appStateManage.setDropdownDisabled(true);
+    if (this.appStateManage.StorageKey.getItem('Editable') == 'Edit') {
+      this.IsNewEntity = false;
+
+      this.DetailsFormTitle = this.IsNewEntity
+        ? 'New Salary Slip Request'
+        : 'Edit Salary Slip Request';
+      this.Entity = SalarySlipRequest.GetCurrentInstance();
+      this.appStateManage.StorageKey.removeItem('Editable');
+      this.Entity.p.UpdatedBy = Number(this.appStateManage.StorageKey.getItem('LoginEmployeeRef'))
+    } else {
+      this.Entity = SalarySlipRequest.CreateNewInstance();
+      SalarySlipRequest.SetCurrentInstance(this.Entity);
+      this.getEmployeeListByCompanyRef();
+    }
+    this.InitialEntity = Object.assign(
+      SalarySlipRequest.CreateNewInstance(),
+      this.utils.DeepCopy(this.Entity)
+    ) as SalarySlipRequest;
+    this.focusInput();
+  }
+
+  focusInput = () => {
+    let txtName = document.getElementById('fromdate')!;
+    txtName.focus();
+  }
+
+  getEmployeeListByCompanyRef = async () => {
+    if (this.companyRef() <= 0) {
+      await this.uiUtils.showErrorToster('Company not Selected');
+      return;
+    }
+    let lst = await Employee.FetchEntireListByCompanyRef(this.companyRef(), async errMsg => await this.uiUtils.showErrorMessage('Error', errMsg));
+    this.EmployeeList = lst;
+    this.Entity.p.EmployeeRef = this.EmployeeList[0].p.Ref
+  }
+
+  SaveSalarySlipRequest = async () => {
+    this.Entity.p.CompanyRef = this.companystatemanagement.getCurrentCompanyRef();
+    this.Entity.p.CompanyName = this.companystatemanagement.getCurrentCompanyName();
+
+    this.Entity.p.FromMonth = this.dtu.ConvertStringDateToFullFormat(this.frommonth);
+    this.Entity.p.ToMonth = this.dtu.ConvertStringDateToFullFormat(this.tomonth);
+    this.Entity.p.FromYear = this.dtu.ConvertStringDateToFullFormat(this.fromyear);
+    this.Entity.p.ToYear = this.dtu.ConvertStringDateToFullFormat(this.toyear);
+
+    if (this.Entity.p.CreatedBy == 0) {
+      this.Entity.p.CreatedBy = Number(this.appStateManage.StorageKey.getItem('LoginEmployeeRef'))
+    }
+    // let entityToSave = this.Entity.GetEditableVersion();
+    // let entitiesToSave = [entityToSave];
+    // let tr = await this.utils.SavePersistableEntities(entitiesToSave);
+
+    // if (!tr.Successful) {
+    //   this.isSaveDisabled = false;
+    //   this.uiUtils.showErrorMessage('Error', tr.Message);
+    //   return;
+    // } else {
+    //   this.isSaveDisabled = false;
+    //   if (this.IsNewEntity) {
+    //     await this.uiUtils.showSuccessToster('SalarySlipRequest Master saved successfully!');
+    //     this.Entity = SalarySlipRequest.CreateNewInstance();
+    //     this.resetAllControls();
+    //   } else {
+    //     await this.uiUtils.showSuccessToster('SalarySlipRequest Master Updated successfully!');
+    //     await this.router.navigate(['/homepage/Website/Salary_Slip_Request']);
+    //   }
+    // }
+  };
+
+  // for value 0 selected while click on Input //
+  selectAllValue(event: MouseEvent): void {
+    const input = event.target as HTMLInputElement;
+    input.select();
+  }
+
+  BackSalarySlipRequest = () => {
+    this.router.navigate(['/homepage/Website/Salary_Slip_Request']);
+  }
+
+  resetAllControls = () => {
+    // reset touched
+    this.NameInputControl.control.markAsUntouched();
+    this.CodeInputControl.control.markAsUntouched();
+
+    // reset dirty
+    this.NameInputControl.control.markAsPristine();
+    this.CodeInputControl.control.markAsPristine();
+  }
 }
