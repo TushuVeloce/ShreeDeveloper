e847f7ca07354a7347f01dcbec83cb2e726e1ca8
UmarMulla17 <99394160+umarmulla@users.noreply.github.com>
2025-04-09 17:46:18 +0530

    code changes in attendance log

---
+++ src/app/classes/domain/entities/website/customer_management/hr_payroll_management/attendancelogsfetchrequest.ts
@@ -0,0 +1,27 @@
+import { TransportData } from 'src/app/classes/infrastructure/transportdata';
+import { DataContainerService } from 'src/app/classes/infrastructure/datacontainer.service';
+import { Utils } from 'src/app/services/utils.service';
+import { RequestTypes } from 'src/app/classes/infrastructure/enums';
+import { DataCollection } from 'src/app/classes/infrastructure/datacollection';
+
+export class AttendanceLogsFetchRequest
+{
+    public static readonly FetchRequestType: string = "AttendanceLogsFetchRequest";
+
+    CompanyRefs: number[] = [];
+    AttendanceLogsRefs: number[] = [];
+
+    public MergeIntoTransportData = (td: TransportData) =>
+    {
+        let coll = DataContainerService.GetInstance().GetOrCreateCollection(td.MainData, AttendanceLogsFetchRequest.FetchRequestType) as DataCollection;
+        coll.Entries.push(this);
+    }
+
+    public FormulateTransportData = () => {
+        let td = Utils.GetInstance().CreateNewTransportData(RequestTypes.Fetch);
+        this.MergeIntoTransportData(td);
+
+        return td;
+    }
+}
+
