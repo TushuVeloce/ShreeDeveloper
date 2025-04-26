b2d60262628a655cfb38202c9b656a4928d6573e
spveloce <sagar.p@velocetechinsights.com>
2025-04-26 14:44:35 +0530

    razorpay integration

---
+++ src/app/classes/infrastructure/razorpay/createorder/createorderresponse.ts
@@ -0,0 +1,34 @@
+import { DataContainerService } from "../../datacontainer.service";
+import { TransportData } from "../../transportdata";
+import { CreateOrderCustomProcessRequest } from "./createordercustomrequest";
+
+export class CreateOrderResponse
+{
+    public static readonly EmbedCollectionName: string = "CreateOrderResponse";
+
+    CustomRequest: CreateOrderCustomProcessRequest = null as any;
+    Rows: any
+
+    public static FromTransportData(td: TransportData)
+    {
+        // let result: CreateOrderResponse = null as any;
+        let result: any = null as any;
+
+        let dcs = DataContainerService.GetInstance();
+
+        if (dcs.CollectionExists(td.MainData, CreateOrderResponse.EmbedCollectionName))
+        {
+            let coll = dcs.GetCollection(td.MainData, CreateOrderResponse.EmbedCollectionName)!;
+            for(let data of coll.Entries)
+            {
+                result = data as any;
+            }
+        }
+        else
+        {
+            //result = new CreateOrderResponse();
+        }
+
+        return result;
+    }
+}
