76b801fc1d75f7d1b615593d3a462c7ab3ed0ce1
MaithileePatil <maithilee.p@velocetechinsights.com>
2025-05-06 17:52:10 +0530

    changes

---
+++ src/app/classes/domain/entities/website/site_management/time/time.ts
@@ -0,0 +1,192 @@
+import { IPersistable } from "src/app/classes/infrastructure/IPersistable";
+import { DataContainer } from "src/app/classes/infrastructure/datacontainer";
+import { DataContainerService } from "src/app/classes/infrastructure/datacontainer.service";
+import { PayloadPacketFacade } from "src/app/classes/infrastructure/payloadpacket/payloadpacketfacade";
+import { TransportData } from "src/app/classes/infrastructure/transportdata";
+import { ValidationResultAccumulator } from "src/app/classes/infrastructure/validationresultaccumulator";
+import { IdProvider } from "src/app/services/idprovider.service";
+import { ServerCommunicatorService } from "src/app/services/server-communicator.service";
+import { Utils } from "src/app/services/utils.service";
+import { isNullOrUndefined } from "src/tools";
+import { UIUtils } from "src/app/services/uiutils.service";
+import { RequestTypes } from "src/app/classes/infrastructure/enums";
+import { TimeFetchRequest } from "./timefetchrequest";
+
+
+export class TimeDetailProps {
+  public readonly Db_Table_Name = "SiteManagementTimeDetails";
+  public Ref: number = 0;
+  public StartTime: string ='';
+  public EndTime : string ='';
+  public WorkedHours : number =0;
+  public SiteManagementRef : number = 0;
+
+  public readonly IsNewlyCreated: boolean = false;
+  // public readonly AccountTypeName: string = '';
+
+  public constructor(isNewlyCreated: boolean) {
+    this.IsNewlyCreated = isNewlyCreated;
+  }
+
+  public static Blank() {
+    return new TimeDetailProps(true);
+  }
+}
+
+export class  Time implements IPersistable<Time> {
+  public static readonly Db_Table_Name: string = 'SiteManagementTimeDetails';
+
+  public constructor(public readonly p: TimeDetailProps, public readonly AllowEdit: boolean) {
+
+  }
+
+  public async EnsurePrimaryKeysWithValidValues(): Promise<void> {
+    if (this.p.Ref === undefined || this.p.Ref === 0) {
+            const newRefs = await IdProvider.GetInstance().GetNextEntityId();
+            // const newRefs = await IdProvider.GetInstance().GetAllocateSingleIds();
+      this.p.Ref = newRefs[0];
+      if (this.p.Ref <= 0) throw new Error("Cannot assign Id. Please try again");
+    }
+  }
+
+  public GetEditableVersion(): Time {
+    let newState: TimeDetailProps = Utils.GetInstance().DeepCopy(this.p);
+    return Time.CreateInstance(newState, true);
+  }
+
+  public static CreateNewInstance() {
+    return new Time(TimeDetailProps.Blank(), true);
+  }
+
+  public static CreateInstance(data: any, allowEdit: boolean) {
+    return new Time(data as TimeDetailProps, allowEdit);
+  }
+
+  public CheckSaveValidity(_td: TransportData, vra: ValidationResultAccumulator): void {
+    if (!this.AllowEdit) vra.add('', 'This object is not editable and hence cannot be saved.');
+  }
+
+  public MergeIntoTransportData(td: TransportData) {
+    DataContainerService.GetInstance().MergeIntoContainer(td.MainData, Time.Db_Table_Name, this.p);
+  }
+
+  private static m_currentInstance: Time = Time.CreateNewInstance();
+
+  public static GetCurrentInstance() {
+    return Time.m_currentInstance;
+  }
+
+  public static SetCurrentInstance(value: Time) {
+    Time.m_currentInstance = value;
+  }
+
+
+  // ********************************************
+  public static cacheDataChangeLevel: number = -1;
+
+  public static SingleInstanceFromTransportData(td: TransportData): Time {
+    let dcs = DataContainerService.GetInstance();
+    if (dcs.CollectionExists(td.MainData, Time.Db_Table_Name)) {
+      for (let data of dcs.GetCollection(td.MainData, Time.Db_Table_Name)!.Entries) {
+        return Time.CreateInstance(data, false);
+      }
+    }
+
+    return null as any;
+  }
+
+  public static ListFromDataContainer(cont: DataContainer,
+    filterPredicate: (arg0: any) => boolean = null as any,
+    sortPropertyName: string = "Name"): Time[] {
+    let result: Time[] = [];
+
+    let dcs = DataContainerService.GetInstance();
+
+    if (dcs.CollectionExists(cont, Time.Db_Table_Name)) {
+      let coll = dcs.GetCollection(cont, Time.Db_Table_Name)!;
+      let entries = coll.Entries;
+
+      if (!isNullOrUndefined(filterPredicate)) entries = entries.filter(filterPredicate);
+
+      if (sortPropertyName.trim().length > 0) {
+        entries = Utils.OrderByPropertyName(entries, sortPropertyName);
+      }
+
+      for (let data of entries) {
+        result.push(Time.CreateInstance(data, false));
+      }
+    }
+
+    return result;
+  }
+
+  public static ListFromTransportData(td: TransportData): Time[] {
+    return Time.ListFromDataContainer(td.MainData);
+  }
+
+  public static async FetchTransportData(req: TimeFetchRequest, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdRequest = req.FormulateTransportData();
+    let pktRequest = PayloadPacketFacade.GetInstance().CreateNewPayloadPacket2(tdRequest);
+
+    let tr = await ServerCommunicatorService.GetInstance().sendHttpRequest(pktRequest);
+    if (!tr.Successful) {
+      if (!isNullOrUndefined(errorHandler)) await errorHandler(tr.Message);
+      return null;
+    }
+
+    let tdResponse = JSON.parse(tr.Tag) as TransportData;
+    return tdResponse;
+  }
+
+  public static async FetchInstance(ref: number, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let req = new TimeFetchRequest();
+    req.TimeRefs.push(ref);
+
+    let tdResponse = await Time.FetchTransportData(req, errorHandler) as TransportData;
+    return Time.SingleInstanceFromTransportData(tdResponse);
+  }
+
+  public static async FetchList(req: TimeFetchRequest, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdResponse = await Time.FetchTransportData(req, errorHandler) as TransportData;
+    return Time.ListFromTransportData(tdResponse);
+  }
+
+  public static async FetchEntireList(errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let req = new TimeFetchRequest();
+    let tdResponse = await Time.FetchTransportData(req, errorHandler) as TransportData;
+    return Time.ListFromTransportData(tdResponse);
+  }
+
+   public static async FetchEntireListByCompanyRefAndSiteRef(CompanyRef:number,SiteRef:number,errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+     let req = new TimeFetchRequest();
+     req.CompanyRefs.push(CompanyRef)
+     req.SiteManagementRefs.push(SiteRef)
+     let tdResponse = await Time.FetchTransportData(req, errorHandler) as TransportData;
+     return Time.ListFromTransportData(tdResponse);
+   }
+
+    public static async FetchEntireListBySiteRef(siteref:number,errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+       let req = new TimeFetchRequest();
+       req.SiteManagementRefs.push(siteref)
+       let tdResponse = await Time.FetchTransportData(req, errorHandler) as TransportData;
+       return Time.ListFromTransportData(tdResponse);
+     }
+   
+
+  public async DeleteInstance(successHandler: () => Promise<void> = null!, errorHandler: (err: string) => Promise<void> = UIUtils.GetInstance().GlobalUIErrorHandler) {
+    let tdRequest = new TransportData();
+    tdRequest.RequestType = RequestTypes.Deletion;
+
+    this.MergeIntoTransportData(tdRequest);
+    let pktRequest = PayloadPacketFacade.GetInstance().CreateNewPayloadPacket2(tdRequest);
+
+    let tr = await ServerCommunicatorService.GetInstance().sendHttpRequest(pktRequest);
+    if (!tr.Successful) {
+      if (!isNullOrUndefined(errorHandler)) await errorHandler(tr.Message);
+    }
+    else {
+      if (!isNullOrUndefined(successHandler)) await successHandler();
+    }
+  }
+
+}
