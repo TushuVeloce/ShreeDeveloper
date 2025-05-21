243d5c62d529a67dff93db542cb07b78346e5057
Ijaj97 <124039753+Ijaj97@users.noreply.github.com>
2025-05-21 18:51:27 +0530

    expense

--- src/app/classes/domain/entities/website/masters/expensetype/expensetype.ts
+++ src/app/classes/domain/entities/website/masters/expensetype/expensetype.ts
@@ -69,6 +69,11 @@ export class ExpenseType implements IPersistable<ExpenseType> {
 
   public CheckSaveValidity(_td: TransportData, vra: ValidationResultAccumulator): void {
     if (!this.AllowEdit) vra.add('', 'This object is not editable and hence cannot be saved.');
+    if (this.p.Name == '') {
+      vra.add('Name', 'Name cannot be blank.');
+    } else if (!new RegExp(ValidationPatterns.NameWithNosAndSpace).test(this.p.Name)) {
+      vra.add('Name', ValidationMessages.NameWithNosAndSpaceMsg + ' for Name');
+    }
   }
 
   public MergeIntoTransportData(td: TransportData) {
@@ -101,7 +106,7 @@ export class ExpenseType implements IPersistable<ExpenseType> {
 
   public static ListFromDataContainer(cont: DataContainer,
     filterPredicate: (arg0: any) => boolean = null as any,
-   sortPropertyName: string = ""): ExpenseType[] {
+    sortPropertyName: string = ""): ExpenseType[] {
     let result: ExpenseType[] = [];
 
     let dcs = DataContainerService.GetInstance();
