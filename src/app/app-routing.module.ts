df45c023b9cf2823e710f13a4bce2d5f924ba566
pranavpvelocetechinsights <pranav.p@velocetechinsights.com>
2025-06-08 13:16:59 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { CreatePasswordComponent } from './views/login-page/create_password/create-password/create-password.component';
+import { ForgotPasswordComponent } from './views/sidebarlayout/forgot_password/forgot-password/forgot-password.component';
 
 const routes: Routes = [
 
-  { path: '', pathMatch: 'full', redirectTo: '/mobileapp' },
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginPageComponent, },
   { path: 'create_password', component: CreatePasswordComponent, },
+  { path: 'forgot_password', component: ForgotPasswordComponent },
   {
     path: 'homepage',
     loadChildren: () => import('./views/sidebarlayout/sidebarlayout.routes').then(m => m.SidebarLayout_ROUTES)
   },
   // ,
   // {
-  //   path: 'app_homepage',
+  //   path: 'app_homepage',a
   //   loadChildren: () => import('./views/mobile-app/mobile.module').then(m => m.MobileModule)
   // },
   {
