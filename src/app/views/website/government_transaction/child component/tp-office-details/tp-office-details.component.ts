f22cb443abf2666bdf4b64defa0cb631c45504c8
jpveloce <jyotirmay.p@velocetechinsights.com>
2025-07-16 12:35:55 +0530

    code changes

--- src/app/views/website/government_transaction/child component/tp-office-details/tp-office-details.component.ts
+++ src/app/views/website/government_transaction/child component/tp-office-details/tp-office-details.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { AppStateManageService } from 'src/app/services/app-state-manage.service';
 
@@ -7,7 +15,6 @@ import { AppStateManageService } from 'src/app/services/app-state-manage.service
   templateUrl: './tp-office-details.component.html',
   styleUrls: ['./tp-office-details.component.scss'],
   standalone: false,
-
 })
 export class TpOfficeDetailsComponent implements OnInit, OnChanges {
   @Input() SelectedTransactionType: string = '';
@@ -15,64 +22,93 @@ export class TpOfficeDetailsComponent implements OnInit, OnChanges {
 
   TpOfficeList: any[] = [];
 
-  constructor(private appStateManage: AppStateManageService, private router: Router,) { }
+  constructor(
+    private appStateManage: AppStateManageService,
+    private router: Router
+  ) {}
   ngOnChanges(changes: SimpleChanges): void {
-    let arr = JSON.parse(this.appStateManage.StorageKey.getItem('TransactionJson') ?? '[]');
+    let arr = JSON.parse(
+      this.appStateManage.StorageKey.getItem('TransactionJson') ?? '[]'
+    );
 
-    this.TpOfficeList = arr.filter((item: { SiteWorkGroupName: string }) => item.SiteWorkGroupName == this.SelectedTransactionType);
+    this.TpOfficeList = arr.filter(
+      (item: { SiteWorkGroupName: string }) =>
+        item.SiteWorkGroupName == this.SelectedTransactionType
+    );
     this.BindApplicableTypeValueStatusToRadioButton();
     console.log(this.TpOfficeList);
-
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   BindApplicableTypeValueStatusToRadioButton = () => {
-
+    debugger;
     for (let i = 0; i < this.TpOfficeList.length; i++) {
       let SiteWorksList = this.TpOfficeList[i].SiteWorks;
-      let ApplicableTypesList = SiteWorksList.filter((item: { SiteWorkName: string }) => item.SiteWorkName == 'Report NOC & Airport NOC');
+      let ApplicableTypesList = SiteWorksList.filter(
+        (item: { SiteWorkName: string }) =>
+          item.SiteWorkName.trim() == 'Report NOC & Airport NOC'
+      );
 
-      let ApplicableTypeValueList = ApplicableTypesList[0].ApplicableTypes.filter((item: { SiteWorkApplicableTypeName: string }) => item.SiteWorkApplicableTypeName == 'Yes No');
-      this.getReportNOCAirportNOC(ApplicableTypeValueList[0].Value, ApplicableTypesList[0].SiteWorkName);
+      let ApplicableTypeValueList =
+        ApplicableTypesList[0].ApplicableTypes.filter(
+          (item: { SiteWorkApplicableTypeName: string }) =>
+            item.SiteWorkApplicableTypeName.trim() == 'Yes No'
+        );
+      this.getReportNOCAirportNOC(
+        ApplicableTypeValueList[0].Value,
+        ApplicableTypesList[0].SiteWorkName
+      );
     }
+  };
 
-  }
-
-  getTypeOnApplicableTypeName = (ApplicableTypesName: string): '' | 'checkbox' | 'number' | 'date' | 'radio' | 'text' => {
+  getTypeOnApplicableTypeName = (
+    ApplicableTypesName: string
+  ): '' | 'checkbox' | 'number' | 'date' | 'radio' | 'text' => {
     switch (ApplicableTypesName) {
-      case 'Submit': return 'checkbox';
-      case 'Inward No': return 'number';
-      case 'Inward Date': return 'date';
-      case 'Outward No': return 'number';
-      case 'Outward Date': return 'date';
-      case 'Scrutiny Fees': return 'checkbox';
-      case 'Yes No': return 'radio';
-      case 'Received': return 'checkbox';
+      case 'Submit':
+        return 'checkbox';
+      case 'Submit to तहसीलदार':
+        return 'checkbox';
+      case 'Submit to उपाधीक्षक & भूमी अधिलेख':
+        return 'checkbox';
+      case 'Inward No':
+        return 'number';
+      case 'Inward Date':
+        return 'date';
+      case 'Outward No':
+        return 'number';
+      case 'Outward Date':
+        return 'date';
+      case 'Scrutiny Fees':
+        return 'checkbox';
+      case 'Yes No':
+        return 'radio';
+      case 'Received':
+        return 'checkbox';
       default:
         return ''; // Default return value
     }
-  }
+  };
 
   // hide show content using radio button
   showReportNOCSection: boolean = false;
   getReportNOCAirportNOC = async (value: boolean, siteWorkName: string) => {
+    debugger;
     if (siteWorkName.trim() === 'Report NOC & Airport NOC') {
       this.showReportNOCSection = value;
     }
-  }
+  };
 
   onSave = () => {
     // this.appStateManage.StorageKey.setItem('TpOfficeList', JSON.stringify(this.TpOfficeList));
     this.onEntitySaved.emit(this.TpOfficeList);
     this.router.navigate(['/homepage/Website/Site_Progress_Report_Details']);
-  }
+  };
 
   onCancel = async () => {
-    await this.router.navigate(['/homepage/Website/Site_Progress_Report_Details']);
-
-  }
-
+    await this.router.navigate([
+      '/homepage/Website/Site_Progress_Report_Details',
+    ]);
+  };
 }
